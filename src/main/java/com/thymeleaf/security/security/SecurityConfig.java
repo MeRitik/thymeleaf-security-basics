89c8a8ad33d9e9b4768695d700beeb78bc01f70b
Ritik Ranjan <ritikranjan999@gmail.com>
2025-03-17 01:34:11 +0530

    Initial commit

---
+++ src/main/java/com/thymeleaf/security/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.thymeleaf.security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                configurer ->
+                        configurer.anyRequest().authenticated()
+        ).formLogin(
+                form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/handleLogin")
+                        .permitAll()
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails user1 = User.builder()
+                .username("john")
+                .password("{noop}cenaaa")
+                .authorities("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("emma")
+                .password("{noop}stones")
+                .authorities("USER", "MANAGER")
+                .build();
+
+        UserDetails user3 = User.builder()
+                .username("buble")
+                .password("{noop}michael")
+                .authorities("USER", "MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2, user3);
+    }
+}

339d5eb26f01e9f18adf8ac9c353354297b25c04
Ritik Ranjan <ritikranjan999@gmail.com>
2025-03-17 12:38:30 +0530

    url and method based restrictions @EnableMethodSecurity

--- src/main/java/com/thymeleaf/security/controller/HomeController.java
+++ src/main/java/com/thymeleaf/security/controller/HomeController.java
@@ -1,13 +1,27 @@
 package com.thymeleaf.security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+//    @PreAuthorize("hasRole('USER')")
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+//    @PreAuthorize("hasRole('MANAGER')")
+    @GetMapping("/leaders")
+    public String leaders() {
+        return "leader-page";
+    }
+
+//    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/systems")
+    public String systems() {
+        return "admin-page";
     }
 }
--- src/main/java/com/thymeleaf/security/security/SecurityConfig.java
+++ src/main/java/com/thymeleaf/security/security/SecurityConfig.java
@@ -2,13 +2,16 @@ package com.thymeleaf.security.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+//@EnableMethodSecurity // For @PreAuthorize
 @Configuration
 public class SecurityConfig {
 
@@ -16,7 +19,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
                 configurer ->
-                        configurer.anyRequest().authenticated()
+                        configurer
+                                .requestMatchers("/").hasRole("USER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .anyRequest().authenticated()
         ).formLogin(
                 form -> form
                         .loginPage("/login")
@@ -34,19 +41,19 @@ public class SecurityConfig {
         UserDetails user1 = User.builder()
                 .username("john")
                 .password("{noop}cenaaa")
-                .authorities("USER")
+                .authorities("ROLE_USER")
                 .build();
 
         UserDetails user2 = User.builder()
                 .username("emma")
                 .password("{noop}stones")
-                .authorities("USER", "MANAGER")
+                .authorities("ROLE_USER", "ROLE_MANAGER")
                 .build();
 
         UserDetails user3 = User.builder()
                 .username("buble")
                 .password("{noop}michael")
-                .authorities("USER", "MANAGER")
+                .authorities("ROLE_USER", "ROLE_MANAGER", "ROLE_ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(user1, user2, user3);
